ad5c02407e24a1057d66385683b622f84019fd52
cybemachine <cybemachine142@gmail.com>
2021-11-13 18:19:20 +0545

    added res.status

--- packages/cybeexpress/res.js
+++ packages/cybeexpress/res.js
@@ -1,7 +1,12 @@
-var { ServerResponse } = require('http');
+var {
+    ServerResponse
+} = require('http');
 
 var res = Object.create(ServerResponse.prototype)
 
-
+res.status = function status(code) {
+    this.statusCode = code;
+    return this;
+};
 
 module.exports = res
